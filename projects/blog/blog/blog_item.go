ec26d57a5fa30e305c96dee89fd448dd2a6bbcca
Tomas De Pietro <tomas94depi@gmail.com>
2024-05-09 12:18:25 -0300

    boilerplate blog project

---
+++ projects/blog/blog/blog_item.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	pbblog "github.com/tomasdepi/golang/projects/blog/pbblog"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type BlogItem struct {
+	ID       primitive.ObjectID `bson:"_id", omitempty`
+	AuthorID string             `bson:"author_id"`
+	Title    string             `bson:"title"`
+	Content  string             `bson:"content"`
+}
+
+func documentToBlog(data *BlogItem) *pbblog.Blog {
+	return &pbblog.Blog{
+		Id:       data.ID.Hex(),
+		AuthorId: data.AuthorID,
+		Title:    data.Title,
+		Contect:  data.Content,
+	}
+}
---
+++ projects/blog/go.mod
@@ -0,0 +1,16 @@
+module github.com/tomasdepi/golang/projects/blog
+
+go 1.22.2
+
+require (
+	google.golang.org/grpc v1.63.2
+	google.golang.org/protobuf v1.33.0
+)
+
+require (
+	go.mongodb.org/mongo-driver v1.15.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240227224415-6ceb2ff114de // indirect
+)
